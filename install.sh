52965578a951d77e7f938111f6a036d70ce4d4f3
wnortman@spireon.com <wnortman@spireon.com>
2016-10-19 16:14:03 -0700

    Working version

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@ mkdir $OPENGROK_INSTANCE_BASE
 mkdir $OPENGROK_INSTANCE_BASE/data
 mkdir $OPENGROK_INSTANCE_BASE/etc
 
-wget -O - https://github.com/OpenGrok/OpenGrok/files/467358/opengrok-0.12.1.6.tar.gz.zip | unzip | tar zxvf -
-mv opengrok-* opengrok
-cd /opengrok/bin
+wget https://github.com/OpenGrok/OpenGrok/files/467358/opengrok-0.12.1.6.tar.gz.zip
+unzip opengrok-0.12.1.6.tar.gz.zip
+tar zxvf opengrok-0.12.1.6.tar.gz
+echo "Move Directory"
+mv opengrok-0.12.1.6 opengrok
+echo "Change Directory"
+cd opengrok/bin
+echo "Deploy OpenGrok"
 ./OpenGrok deploy
