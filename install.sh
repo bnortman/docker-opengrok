87996810b3f91dfecab84f9b4aa14ab1489c148f
wnortman@spireon.com <wnortman@spireon.com>
2016-10-19 15:54:05 -0700

    Updating container

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ mkdir $OPENGROK_INSTANCE_BASE
 mkdir $OPENGROK_INSTANCE_BASE/data
 mkdir $OPENGROK_INSTANCE_BASE/etc
 
-wget -O - https://java.net/projects/opengrok/downloads/download/opengrok-0.12.1.tar.gz | tar zxvf -
+wget -O - https://github.com/OpenGrok/OpenGrok/files/467358/opengrok-0.12.1.6.tar.gz.zip | unzip | tar zxvf -
 mv opengrok-* opengrok
 cd /opengrok/bin
 ./OpenGrok deploy
